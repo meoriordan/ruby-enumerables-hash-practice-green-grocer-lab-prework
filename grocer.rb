bed503727a9c99733c92634c482c5a6f655d9a82
Elizabeth ORiordan <elizabeth.oriordan@flatironschool.com>
2020-03-01 21:31:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,5 +41,8 @@ end
 
 def checkout(cart, coupons)
   cart = consolidate_cart(cart)
-  cart = 
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  total_price = 0 
+  cart.each 
 end
